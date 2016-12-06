50fabe11cbfc7e03e0c47f6d83ccc9070fa53f04
acamilo <alex.camilo@gmail.com>
2016-12-05 22:22:22 -0500

    Populate EEPROM config section from admin.json

--- DoorEntryESP-FW/CardDB.cpp
+++ DoorEntryESP-FW/CardDB.cpp
@@ -3,7 +3,8 @@
 #include <FS.h>
 
 
-
+#define eepromRead  dummyRead
+#define eepromWrite dummyWrite
 uint8_t dummyEEPROM[2048];
 
 bool updateEEPROM(String cfg){
@@ -77,16 +78,70 @@ bool updateEEPROM(String cfg){
     eeprecordindex++;
   }
   db.close();
-  int cc=0;
-  for (int i=0; i<2046; i++) {
-    print_hex(dummyEEPROM[i],8);
-    Serial.print(" ");
-    cc++;
-    if (cc>15){
-      Serial.println("");
-      cc=0;
-      }
-    }
+    return true;
+}
+
+bool updateEEPROMConfig(String cfg){
+  File db = SPIFFS.open(cfg, "r");
+  if (db==0) return false;
+  char buf[1024];
+  for(int i=0; i<sizeof(buf); i++) buf[i]=0;
+  uint32_t filesize = db.readBytes(buf,sizeof(buf));
+  buf[filesize+1]=0;
+  // Parse object w json parser.
+  DynamicJsonBuffer jsonBuffer;
+  JsonObject& root = jsonBuffer.parseObject(buf);
+  printCardConfigInfo(root);
+
+  reader_config rc;
+  for(int i=0; i<32; i++) rc.b[i]=0;
+
+    rc.f.sitestart=(int)root["sitestartbit"];
+    rc.f.sitelen=(int)root["sitelength"];
+    rc.f.idstart=(int)root["idstartbit"];
+    rc.f.idlen=(int)root["idlength"];
+      
+    rc.f.partype1=strcmp((const char*)root["parity1type"],"on")?0:1;;
+    rc.f.parbit1=(int)root["parity1pos"];
+    rc.f.parstart1=(int)root["parity1start"];
+    rc.f.parlen1=(int)root["parity1bits"];
+      
+    rc.f.partype2=strcmp((const char*)root["parity2type"],"on")?0:1;
+    rc.f.parbit2=(int)root["parity1pos"];
+    rc.f.parstart2=(int)root["parity2start"];
+    rc.f.parlen2=(int)root["parity2bits"];
+    
+    rc.f.ignoreparity=strcmp((const char*)root["ignoreparity"],"on")?0:1;
+    rc.f.ignoresite=strcmp((const char*)root["ignoresitecode"],"on")?0:1;
+    rc.f.ignoreid=0;
+    rc.f.rsvd1=0;
+
+    for(int i=0; i<32; i++) eepromWrite(i,rc.b[i]);
+
+  
+ 
+  db.close();
+}
+
+void printCardConfigInfo(JsonObject& root){
+  Serial.println("");
+  Serial.println("Card Config");
+  Serial.print("  ignoreparity: "); Serial.println((const char*)root["ignoreparity"]);
+  Serial.print("  ignoresitecode: "); Serial.println((const char*)root["ignoresitecode"]);
+  Serial.print("  logfailures: "); Serial.println((const char*)root["logfailures"]);
+  Serial.print("  sitestartbit: "); Serial.println((int)root["sitestartbit"]);
+  Serial.print("  sitelength: "); Serial.println((int)root["sitelength"]);
+  Serial.print("  idstartbit: "); Serial.println((int)root["idstartbit"]);
+  Serial.print("  idlength: "); Serial.println((int)root["idlength"]);
+  Serial.print("  parity1type: "); Serial.println((const char*)root["parity1type"]);
+  Serial.print("  parity1pos: "); Serial.println((int)root["parity1pos"]);
+  Serial.print("  parity1bits: "); Serial.println((int)root["parity1bits"]);
+  Serial.print("  parity1start: "); Serial.println((int)root["parity1start"]);
+  Serial.print("  parity2type: "); Serial.println((const char*)root["parity2type"]);
+  Serial.print("  parity2pos: "); Serial.println((int)root["parity2pos"]);
+  Serial.print("  parity2bits: "); Serial.println((int)root["parity2bits"]);
+  Serial.print("  parity2start: "); Serial.println((int)root["parity2start"]);
+  
 }
 
 void printCardInfo(JsonObject& root){
@@ -121,8 +176,8 @@ void printCardRecord(card_record c){
    Serial.print("    ");
   
    Serial.print(numtoWeekString(c.f.hours[i].f.rday));Serial.print(": ");
-   Serial.print(c.f.hours[i].f.rtimein,HEX);Serial.print(" to ");
-   Serial.println(c.f.hours[i].f.rtimeout,HEX);
+   Serial.print(c.f.hours[i].f.rtimein);Serial.print(" to ");
+   Serial.println(c.f.hours[i].f.rtimeout);
   
  }
  Serial.println("");
@@ -171,8 +226,7 @@ void dummyWrite(uint16_t addr,uint8_t data){
   dummyEEPROM[addr]=data;
 }
 
-#define eepromRead  dummyRead
-#define eepromWrite dummyWrite
+
 
 card_record dbGetCard(uint16_t index){
   card_record card;
@@ -181,6 +235,19 @@ card_record dbGetCard(uint16_t index){
 }
 void dbSetCard(card_record r, uint16_t index){
   for(int i=0; i<16; i++) eepromWrite((index*16)+i,r.b[i]);  
+}
+
+void dumpEEPROM(){
+      int cc=0;
+  for (int i=0; i<2046; i++) {
+    print_hex(dummyEEPROM[i],8);
+    Serial.print(" ");
+    cc++;
+    if (cc>15){
+      Serial.println("");
+      cc=0;
+      }
+    }
 }
 
 void print_hex(int v, int num_places)
--- DoorEntryESP-FW/CardDB.h
+++ DoorEntryESP-FW/CardDB.h
@@ -13,6 +13,8 @@
 #define sunday    0x9
 #define everyday  0xa
 
+
+
 typedef struct{
   unsigned int   rday:4;
   unsigned int   rtimein:6;
@@ -37,9 +39,44 @@ typedef union{
   uint8_t       b[16];
 } card_record;
 
-bool updateEEPROM(String cfg);
-void printCardInfo(JsonObject& root);
+// Reader config section.
+typedef struct{
+  uint8_t   sitestart;
+  uint8_t   sitelen;
+  uint8_t   idstart;
+  uint8_t   idlen;
+  
+  uint8_t   partype1;
+  uint8_t   parbit1;
+  uint8_t   parstart1;
+  uint8_t   parlen1;
+  
+  uint8_t   partype2;
+  uint8_t   parbit2;
+  uint8_t   parstart2;
+  uint8_t   parlen2;
 
+  uint8_t   ignoreparity;
+  uint8_t   ignoresite;
+  uint8_t   ignoreid;
+  uint8_t   rsvd1;
+
+  timedur   openhours[8];
+} __attribute__((packed, aligned(1))) reader_config_s;
+
+typedef union{
+  reader_config_s f;
+  uint8_t  b[32];
+} reader_config;
+
+
+
+bool updateEEPROM(String cfg);
+bool updateEEPROMConfig(String cfg);
+void printCardInfo(JsonObject& root);
+void printCardConfigInfo(JsonObject& root);
+
+void dumpEEPROM();
 void printCardRecord(card_record c);
 uint8_t weekStringToNum(const char* string);
 char * numtoWeekString(uint8_t i);
